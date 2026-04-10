b7bd31a5560da9e8d485c99ad892a41a117e42da
ramsesyok <ramses-studie@memoad.jp>
2026-04-11 07:08:51 +0900

    SERVER_IDが取得できなかった時のフォールバックを追加

--- server/main.go
+++ server/main.go
@@ -11,6 +11,18 @@ import (
 	"google.golang.org/grpc"
 )
 
+// localIPWithPort はアウトバウンド接続で使われるローカル IP アドレスを取得し、
+// "IP:port" 形式の文字列を返します。取得できない場合は "unknown:port" を返します。
+func localIPWithPort(port string) string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "unknown:" + port
+	}
+	defer conn.Close()
+	addr := conn.LocalAddr().(*net.UDPAddr)
+	return addr.IP.String() + ":" + port
+}
+
 // main は gRPC サーバーを起動します。
 // TCP ポート 50051 でリッスンし、SimulationService を登録します。
 func main() {
@@ -20,7 +32,7 @@ func main() {
 	}
 	serverID := os.Getenv("SERVER_ID")
 	if serverID == "" {
-		serverID = "unknown"
+		serverID = localIPWithPort("50051")
 	}
 	s := grpc.NewServer()
 	simpb.RegisterSimulationServiceServer(s, &simServer{
